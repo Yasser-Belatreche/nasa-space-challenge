8a793d732bdfb74f0b79fad89d70531d488bf250
Yasser-Belatreche <yasser.belatreche0@gmail.com>
2022-10-01 15:36:12 +0100

    first commit

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+    arrowParens: 'avoid',
+    bracketSameLine: false,
+    bracketSpacing: true,
+    singleQuote: true,
+    trailingComma: 'all',
+    tabWidth: 4,
+    printWidth: 100,
+    endOfLine: 'crlf',
+};
